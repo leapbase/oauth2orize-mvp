db84157ef80db95188401c56c64a733663cb874c
leapbase <yliu0@leapbase.com>
2019-09-15 10:13:18 -0400

    cleanup

--- oauth2orize-consumer-example/passport-example/profile.js
+++ oauth2orize-consumer-example/passport-example/profile.js
@@ -1,10 +1,9 @@
 'use strict';
 
 module.exports.parse = function (json) {
-  var profile = {}
-    ;
-
+  var profile = {};
   profile.id = json.currentUserId;
+  
   /*
   profile.id = json.identity.id;
   profile.displayName = json.identity.first_name + ' ' + json.identity.last_name;
@@ -12,6 +11,6 @@ module.exports.parse = function (json) {
                    givenName: json.identity.first_name };
   profile.emails = [{ value: json.identity.email_address }];
   */
-      
+  
   return profile;
 };
